375a3801538f6ea791515750fac194f6427c3394
stormer3428 <stormer3428@DESKTOP-SLCEC97>
2021-02-21 15:03:41 +0100

    A simple and lightweight plugin allowing to get rid of the slowing down when crouched and when crawling

---
+++ FastCrawler/src/fr/stormer3428/fastcrawl/FC.java
@@ -0,0 +1,47 @@
+package fr.stormer3428.fastcrawl;
+
+import java.util.ArrayList;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FC extends JavaPlugin implements CommandExecutor, Listener{
+
+	@Override
+	public void onEnable() {
+		getCommand("fastcrawl").setExecutor(this);
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	private static ArrayList<Player> crawlers = new ArrayList<Player>();
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(sender instanceof Player) {
+			Player p = (Player) sender;
+			if(cmd.getName().equalsIgnoreCase("fastcrawl")) {
+				if(crawlers.contains(p)) {
+					crawlers.remove(p);
+				}else crawlers.add(p);
+			}
+		}
+		return false;
+	}
+
+	@EventHandler
+	public void onMove(PlayerMoveEvent e) {
+		if(crawlers.contains(e.getPlayer())) {
+			Player p = e.getPlayer();
+			if(p.getBoundingBox().getHeight() <= 0.61) p.setWalkSpeed(0.8f);
+			else if(p.getBoundingBox().getHeight() <= 1.51) p.setWalkSpeed(0.6f);
+			else p.setWalkSpeed(0.2f);
+
+		}
+	}
+}
