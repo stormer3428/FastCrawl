13790a0a327c40ff5f56f4ed1d454e2c21bc18b3
stormer3428 <azertyuiopcharles@gmail.com>
2023-09-17 17:18:24 +0200

    a

--- FastCrawler/src/fr/stormer3428/fastcrawl/FC.java
+++ FastCrawler/src/fr/stormer3428/fastcrawl/FC.java
@@ -3,7 +3,6 @@ package fr.stormer3428.fastcrawl;
 import java.util.ArrayList;
 
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,7 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FC extends JavaPlugin implements CommandExecutor, Listener{
+public class FC extends JavaPlugin implements Listener{
 
 	@Override
 	public void onEnable() {
